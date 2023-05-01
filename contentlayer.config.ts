45cf537aeb308a4aa266593141bceb54f1d90055
Stephane-Pires <stephane.pires.learning@gmail.com>
2023-05-01 12:31:48 +0200

    CODE-BLOCK : add support via shiki

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,5 +1,6 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
 import rehypeKatex from "rehype-katex"
+import rehypePrettyCode from "rehype-pretty-code"
 import remarkMath from "remark-math"
 
 export const Content = defineDocumentType(() => ({
@@ -46,11 +47,39 @@ export const Content = defineDocumentType(() => ({
   },
 }))
 
+// https://rehype-pretty-code.netlify.app/
+const options = {
+  // Use one of Shiki's packaged themes
+  // theme: "one-dark-pro",
+  theme: "github-dark",
+
+  // Keep the background or use a custom background color?
+  keepBackground: true,
+
+  // Callback hooks to add custom logic to nodes when visiting
+  // them.
+  onVisitLine(node: any) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: "text", value: " " }]
+    }
+  },
+  onVisitHighlightedLine(node: any) {
+    // Each line node by default has `class="line"`.
+    node.properties.className.push("highlighted")
+  },
+  onVisitHighlightedWord(node: any) {
+    // Each word node has no className by default.
+    node.properties.className = ["word"]
+  },
+}
+
 export default makeSource({
   contentDirPath: "content",
   documentTypes: [Content],
   mdx: {
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [rehypeKatex, [rehypePrettyCode, options]],
     remarkPlugins: [remarkMath],
   },
 })
