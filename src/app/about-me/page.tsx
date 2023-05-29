2a6b54e1ce4ee38ad772188374700d39ae7c8717
Stephane-Pires <stephane.pires.learning@gmail.com>
2023-05-29 11:05:20 +0200

    Marine advise : "The progress bar is too big"

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -59,7 +59,7 @@ export default function AboutMe() {
     <main className="flex min-h-screen flex-col items-center p-4 pt-20 sm:p-10">
       {/* progress bar */}
       <div
-        className={`fixed left-0 top-20 hidden h-2 bg-gradient-to-r from-accent-500 to-accent-300 sm:flex `}
+        className={`fixed left-0 top-20 hidden h-1 bg-gradient-to-r from-accent-500 to-accent-300 sm:flex `}
         style={{
           width: `calc((${percentScrolled} * 100vw) / 100)`,
         }}
