02607c031532565d42f732acb00f0a62678ca6ba
Stéphane <stephane.pires.learning@gmail.com>
2023-06-16 15:03:02 +0200

    Home Page (logo)  : custom typewriter instead of logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Image from "next/image"
+import Typewriter from "typewriter-effect"
 
 // const inter = Inter({ subsets: ["latin"] })
 
@@ -8,15 +11,36 @@ export default function Home() {
       <div className="row-span-2 flex flex-row items-center justify-around">
         <div className="flex flex-col">
           <h1 className="bg-gradient-to-r from-primary-700 to-primary-300 bg-clip-text font-script text-6xl  font-bold  text-transparent sm:text-8xl">
-            Caravel
+            <Typewriter
+              options={{
+                strings: ["Caravel"],
+                autoStart: true,
+                loop: false,
+                deleteSpeed: Infinity,
+              }}
+            />
           </h1>
           <h2 className="font-script text-xl text-primary-200 sm:text-2xl">
-            For Adventurers
+            <Typewriter
+              options={{
+                strings: [
+                  "For Adventurers",
+                  "For Coders",
+                  "For Challengers",
+                  "For players",
+                  "For Bikers",
+                  "For Climbers",
+                  "For Gamers",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </h2>
         </div>
-        <div className="mr-20 hidden sm:block">
+        {/* <div className="mr-20 hidden sm:block">
           <Image src="/logo.png" alt="logo" width={500} height={500}></Image>
-        </div>
+        </div> */}
       </div>
     </main>
   )
