cb873a45b1bc493908ec5fd085a852e099c64cf1
Stéphane Pires <stephane.pires.learning@gmail.com>
2025-01-28 20:28:31 +0100

    fix(emoji): :bug: remove emojis from the typewritter (not working perfectly)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import { useTypewriter } from "@/hooks/typewritter"
 export default function Home() {
   const text = useTypewriter(
     [
-      "For Developers 💻",
-      "For Adventurers ✈️",
-      "For France 🇫🇷",
-      "For Maps Lovers 🗺️",
-      "For Water Lovers 💧",
+      "For Developers",
+      "For Adventurers",
+      "For France",
+      "For Maps Lovers",
+      "For Water Lovers",
     ],
     {
       speed: 150,
@@ -24,7 +24,7 @@ export default function Home() {
     <main className="grid min-h-screen grid-rows-2 p-8 sm:p-24">
       <div className="row-span-2 flex flex-row items-center justify-around">
         <div className="flex flex-col">
-          <h1 className="bg-linear-to-r from-primary-700 to-primary-300 bg-clip-text font-script text-6xl font-bold text-transparent sm:text-8xl">
+          <h1 className="from-primary-700 to-primary-300 font-script bg-linear-to-r bg-clip-text text-6xl font-bold text-transparent sm:text-8xl">
             Caravel
           </h1>
 
