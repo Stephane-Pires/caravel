1a9d46824981bb524df5506cb02efa3fe5a59c99
Stephane-Pires <stephane.pires.learning@gmail.com>
2025-04-09 22:54:06 +0200

    evol(app): dev-58 🔄 Add scope for commit and change formatting
    Changelog: evolution

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,11 @@ const ERROR_LEVEL = 2
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [ERROR_LEVEL, "always", ["app", "content", "curriculum"]],
+    "scope-enum": [
+      ERROR_LEVEL,
+      "always",
+      ["app", "content", "curriculum", "changeset"],
+    ],
     "type-enum": [
       ERROR_LEVEL,
       "always",
